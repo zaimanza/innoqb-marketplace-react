e21f37017dd163df1972450c2a7bf616b2409d27
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-08-12 14:29:34 +0800

    products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiOutlineHeart, AiOutlineShoppingCart, AiOutlineUser, AiOutlineSearch } from "react-icons/ai"
 import { GiHamburgerMenu } from "react-icons/gi"
 import { RiShoppingBagLine } from "react-icons/ri"
 import { BiUserCircle } from "react-icons/bi"
+const axios = require('axios').default;
 
 const App = () => {
+
+  useEffect(() => {
+    const initFunction = async () => {
+      const products = (await axios?.get(
+        `https://dummyjson.com/products`,
+      ))?.data?.products
+      console.log(products)
+    }
+    initFunction()
+
+    return () => { }
+  }, [])
+
   return (
     <>
       <div className='hidden md:flex h-[4rem] w-full bg-[#1B1D93] justify-center items-center'>
@@ -16,10 +30,10 @@ const App = () => {
               alt="innoqb" />
           </div>
           <div className='mx-auto min-w-[1rem]' />
-          <div class="relative w-full max-w-[30rem]">
-            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Search" />
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+          <div className="relative w-full max-w-[30rem]">
+            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Search" />
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
             </div>
           </div>
           <div className='mx-auto min-w-[1rem]' />
